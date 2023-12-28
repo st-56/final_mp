39832a94f57f5cdc3213df0b1d615ad254f23cff
jess <130740598+jes109@users.noreply.github.com>
2023-12-29 07:01:49 +0800

    Update about.js

--- js/about.js
+++ js/about.js
@@ -12,4 +12,15 @@ $(document).ready(function(){
             $(".nav_icon").attr("menu_click","false");
         }
     });
+    let count=0;
+    $(".guide_pic").click(function(){
+        $(this).attr("src","erro");
+        $(this).css("borderRadius","0");
+        $(".ya").css('dispaly',"block");
+        count++;
+        console.log(count);
+        if(count==4){
+            $(".ya").fadeIn('slow');
+        }
+    });
 })
